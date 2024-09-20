ca6da57259504327a75ea8bbf00409f6809afa67
SagarTeotia1 <147220406+SagarTeotia1@users.noreply.github.com>
2024-09-21 00:21:40 +0530

    commit final

--- src/CreateGarden.js
+++ src/CreateGarden.js
@@ -5,18 +5,18 @@ const CreateGarden = () => {
   const handleClick = (event) => {
     event.preventDefault();
     window.open(
-      "https://www.spatial.io/s/Paath-Shala-655c8e28c41d46b41c94c3e3?share=0",
+      "https://www.spatial.io/s/Players-Home-6619708170f7d10e5463d1da",
       "_blank"
     );
   };
 
   return (
     <div className="fullscreen-container">
-      <h1 className="text-4xl font-bold">Join Our Virtual Garden</h1>
+      <h1 className="text-4xl font-bold">Players Room</h1>
       
       <div className="iframe-container">
         <iframe
-          src="https://www.spatial.io/embed/HERBAL-GARDEN-66c99c7bec47a4dfc7e6effc?share=1870790331538640422"
+          src="https://www.spatial.io/s/Players-Home-6619708170f7d10e5463d1da"
           allow="camera; fullscreen; autoplay; display-capture; microphone; clipboard-write"
           title="Virtual Garden"
         ></iframe>
